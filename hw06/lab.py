03aac8eb6bbb22e381369dad0a8d929a439c410b
snady <sfang2016@gmail.com>
2016-03-31 14:05:48 -0400

    update

--- hw06/lab.py
+++ hw06/lab.py
@@ -17,6 +17,7 @@ def validate(s):
 + uppercase
 + lowercase
 + number
++ alphanum
 1 point for multiple characters
 +
 +
@@ -25,7 +26,6 @@ def validate(s):
 +
 +
 +
-1 point for alphanumeric character
 '''
 def strength(s):
 	uc = [ x for x in s if x in UC_LETTERS ]
@@ -33,13 +33,18 @@ def strength(s):
 	n = [ x for x in s if x in NUMS ]
 	an = [ x for x in s if x in ALPHANUMS ]
 
-	u_uc = [x for x in uc if x.count(x) > 1]
-	u_lc = [x for x in lc if x.count(x) > 1]
-	u_n = [x for x in nc if x.count(x) > 1]
+	u_uc = set(uc)
+	u_lc = set(lc)
+	u_n = set(n)
 
-	if len(uc)
+	master = [uc,lc,n,an,uc,lc,n,u_uc,u_lc,u_n]
 
-print validate('pasta')
-print validate('HELLO PASTA')
-print validate('Hello World111')
-print validate('5343232')
+	strength = [ 1 for subl in master if len(subl) > 0 or len(subl) > 1 ]
+
+	print sum(strength)
+
+
+print strength('pasta')
+print strength('HELLO PASTA')
+print strength('Hello World111')
+print strength('5343232')
